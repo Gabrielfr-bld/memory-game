620dc4c16214f9b61b36fe22cda3ddf6283694d4
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-06-19 21:37:28 -0300

    feat: implements function flipCard

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,20 @@ const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+
+function flipCard() {
+  if (lockBoard) return;
+  
+  if (this === firstCard) return;
+  
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
